19aec6df0a48626f975218d6dcc580f1b802ce00
Random <justarandomlucas@gmail.com>
2023-10-03 14:50:18 +0300

    Update config.js

--- src/assets/data/config.js
+++ src/assets/data/config.js
@@ -1,11 +1,12 @@
 import { readable } from 'svelte/store';
 
-const exponentialRate = 1.5
+const exponentialRate = 1.1
 
 const config = readable({
     ExponentialRate: exponentialRate,
     formulaFunction: (level, basePrice) => {
-        return Math.floor((level+1)**exponentialRate + basePrice)
+       // return Math.floor((level+1)**exponentialRate + basePrice)
+       return Math.floor(basePrice*((level+1)**exponentialRate))
     }
 });
 
